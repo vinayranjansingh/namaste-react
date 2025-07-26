911c2cf3337e55ce52b18e906845b29b6387d2e1
Vinay Ranjan Singh <vinay.singh@iot83.com>
2025-07-26 17:48:56 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
-
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading" },
-//     "Hello World from React !"
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div",
@@ -11,9 +7,7 @@ const parent = React.createElement(
     React.createElement(
         "div",
         { id: "child" },
-        [   React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h2", {}, "I am h2 tag")
-        ]
+        React.createElement("h1", {}, "I am h1 tag")
     ));
   
 const root = ReactDOM.createRoot(document.getElementById("root"));
